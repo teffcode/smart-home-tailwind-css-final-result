f08149878681304b5ec7113713aa0e025f7386c3
Estefany Aguilar <tefy.1@live.com>
2022-03-20 15:24:19 -0500

    ✨ Add: Tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ['./public/index.html', './src/**/*.{html,js}'],
+  theme: {
+      extend: {
+        backgroundImage: {
+          'plant': "url('https://images.pexels.com/photos/916339/pexels-photo-916339.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260')",
+        }
+      },
+  },
+  plugins: [],
+}
